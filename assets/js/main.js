5477061f52084d57d59447d2be9bf0df82b4eeec
𝕭𝖗𝖚𝖒𝖊 <contact@noasecond.com>
2026-01-19 20:22:26 +0100

    ✨ Add warning for small screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,13 @@ import { InputMapper } from './InputMapper.js';
 
 window.addEventListener('DOMContentLoaded', () => {
     window.app = new InputMapper();
+
+    // Mobile warning logic
+    const mobileWarning = document.getElementById('mobileWarning');
+    const closeBtn = document.getElementById('closeWarning');
+    if (closeBtn && mobileWarning) {
+        closeBtn.addEventListener('click', () => {
+            mobileWarning.style.display = 'none';
+        });
+    }
 });
